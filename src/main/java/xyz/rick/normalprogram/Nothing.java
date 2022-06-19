82cc14a2f807bbdf903ba00f8c2132f0990c6709
Rick <rick@abc.xyz>
2022-06-19 19:08:00 +0200

    Initial commit

---
+++ src/main/java/xyz/rick/normalprogram/Nothing.java
@@ -0,0 +1,38 @@
+package xyz.rick.normalprogram;
+
+import javax.sound.sampled.*;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.concurrent.CountDownLatch;
+
+
+public class Nothing {
+
+    static {
+        try {
+            BufferedInputStream stream = new BufferedInputStream(new FileInputStream(new File(Nothing.class.getClassLoader().getResource("rick").toURI())));
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(stream);
+            Clip song = AudioSystem.getClip();
+            song.open(audioIn);
+            song.start();
+
+            final CountDownLatch latch = new CountDownLatch(1);
+            song.addLineListener(new LineListener() {
+                @Override
+                public void update(LineEvent event) {
+                    if (event.getType().equals(LineEvent.Type.STOP)) {
+                        event.getLine().close();
+                        latch.countDown();
+                    }
+                }
+            });
+            latch.await();
+        } catch (Exception ignored) {ignored.printStackTrace();}
+
+
+    }
+
+    public static void main(String[] args){}
+
+}
